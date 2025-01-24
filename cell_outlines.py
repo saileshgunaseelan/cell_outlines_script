9384f40a3e5d4efea797d250f377c501c05c8043
Sailesh Gunaseelan <sailesh.gunaseelan@gmail.com>
2025-01-23 18:15:03 -0800

    Revert formatting

--- cell_outlines.py
+++ cell_outlines.py
@@ -24,7 +24,7 @@ def draw_cell_coordinates_from_txt(file_path: str, image_width: int, image_heigh
     # Initialize a list to hold cells; each cell contains a list of coordinates (and each coordinate is a tuple).
     cell_data = []
     # Read txt, collect cell and cell's coordinates
-    with open(file_path, "r") as file:
+    with open(file_path, 'r') as file:
         # Read the file's content
         content = file.read()
         # Collect all cells
@@ -39,7 +39,7 @@ def draw_cell_coordinates_from_txt(file_path: str, image_width: int, image_heigh
             # Create a list of tuples from the coordinates
             cell_coordinates = []
             # Split the coordinates by ',' and iterate over pairs
-            current_cell = current_cell.split(",")
+            current_cell = current_cell.split(',')
             for coordinate in range(0, len(current_cell), 2):
                 # Collect x-coordinate
                 x_coordinate = int(current_cell[coordinate])
@@ -84,9 +84,7 @@ def draw_cell_coordinates_from_txt(file_path: str, image_width: int, image_heigh
     cell_outlines = cell_outlines.crop((x_min, y_min, x_max, y_max))
 
     # Save and display
-    cell_outlines.save(
-        "images/cell_outlines_from_txt.png"
-    )  # Rename the image as necessary
+    cell_outlines.save("images/cell_outlines_from_txt.png")  # Rename the image as necessary
     cell_outlines.show()
 
 
@@ -101,9 +99,7 @@ def draw_cell_coordinates_from_csv(file_path: str, image_width: int, image_heigh
     pre_cyto_outlines_df = pd.read_csv(file_path)
 
     # Load predicted values
-    predicted_value_excel = load_predicted_values(
-        "csv_data/predicted value_unicell and multicell.xlsx"
-    )
+    predicted_value_excel = load_predicted_values("csv_data/predicted value_unicell and multicell.xlsx")
 
     # Load palette and font, prepare image for eventual coloring
     color_palette = load_gradient_and_bar(cell_outlines, image_width, image_height)
@@ -115,8 +111,8 @@ def draw_cell_coordinates_from_csv(file_path: str, image_width: int, image_heigh
         # Assigning column coordinates based on cell
         # cell 1: (X1, Y1)
         # cell 2: (X2, Y2)...
-        x_column = "X" + str(cell)
-        y_column = "Y" + str(cell)
+        x_column = 'X' + str(cell)
+        y_column = 'Y' + str(cell)
 
         # Creating an empty list to store cell's (x, y) coordinates from .csv file
         cell_coordinates = []
@@ -126,10 +122,7 @@ def draw_cell_coordinates_from_csv(file_path: str, image_width: int, image_heigh
         skip_cell = False
 
         # Iterating through columns X[cell#] and Y[cell#] to collect cell's (x, y) coordinates
-        if (
-            pre_cyto_outlines_df.at[0, x_column] != 0
-            and pre_cyto_outlines_df.at[0, y_column] != 0
-        ):
+        if pre_cyto_outlines_df.at[0, x_column] != 0 and pre_cyto_outlines_df.at[0, y_column] != 0:
             for row_index in range(0, len(pre_cyto_outlines_df)):
                 x_coordinate = pre_cyto_outlines_df.at[row_index, x_column]
                 y_coordinate = pre_cyto_outlines_df.at[row_index, y_column]
@@ -172,17 +165,13 @@ def draw_cell_coordinates_from_csv(file_path: str, image_width: int, image_heigh
             print("Drawing cell #" + str(cell))
 
             # Gradient
-            color_cell(
-                draw, color_palette, cell, cell_coordinates, predicted_value_excel
-            )
+            color_cell(draw, color_palette, cell, cell_coordinates, predicted_value_excel)
 
             # Labeling
             label_cell(draw, font, cell, cell_coordinates)
 
     # Save and display
-    cell_outlines.save(
-        "images/cell_outlines_from_csv.png"
-    )  # Rename the image as necessary
+    cell_outlines.save("images/cell_outlines_from_csv.png")  # Rename the image as necessary
     cell_outlines.show()
 
 
@@ -195,22 +184,20 @@ def load_predicted_values(file_path: str):
     # Identifying the minimum and maximum values of the predicted cyto column
     minimum_predicted_cyto = multicell_sheet_excel["predicted cyto"].min()
     maximum_predicted_cyto = multicell_sheet_excel["predicted cyto"].max()
-    # Adding the column 'scaled predicted cyto' to DataFrame which scales 'predicted cyto' to the range (0, 1).
-    multicell_sheet_excel["scaled predicted cyto"] = (
-        multicell_sheet_excel["predicted cyto"] - minimum_predicted_cyto
-    ) / (maximum_predicted_cyto - minimum_predicted_cyto)
+    # Adding the column "scaled predicted cyto" to DataFrame which scales "predicted cyto" to the range (0, 1).
+    multicell_sheet_excel["scaled predicted cyto"] = (multicell_sheet_excel["predicted cyto"] - minimum_predicted_cyto) / (maximum_predicted_cyto - minimum_predicted_cyto)
 
     # Another example of loading predicted values (unicell)
     # # Loading unicell into a DataFrame
     # unicell_file_path = file_path
 
     # # Modify DataFrame as necessary
-    # unicell_sheet_excel = pd.read_excel(unicell_file_path, sheet_name='unicell')
+    # unicell_sheet_excel = pd.read_excel(unicell_file_path, sheet_name="unicell")
     # # Identifying the minimum and maximum values of the predicted nuc column
-    # minimum_predicted_nuc = unicell_sheet_excel['predicted nuc'].min()
-    # maximum_predicted_nuc = unicell_sheet_excel['predicted nuc'].max()
-    # # Adding the column 'scaled predicted nuc' to DataFrame which scales 'predicted nuc' to the range (0, 1).
-    # unicell_sheet_excel['scaled predicted nuc'] = (unicell_sheet_excel['predicted nuc'] - minimum_predicted_nuc) / (maximum_predicted_nuc - minimum_predicted_nuc)
+    # minimum_predicted_nuc = unicell_sheet_excel["predicted nuc"].min()
+    # maximum_predicted_nuc = unicell_sheet_excel["predicted nuc"].max()
+    # # Adding the column "scaled predicted nuc" to DataFrame which scales "predicted nuc" to the range (0, 1).
+    # unicell_sheet_excel["scaled predicted nuc"] = (unicell_sheet_excel["predicted nuc"] - minimum_predicted_nuc) / (maximum_predicted_nuc - minimum_predicted_nuc)
 
     return multicell_sheet_excel
 
@@ -227,52 +214,33 @@ def load_gradient_and_bar(cell_outlines: Image, image_width: int, image_height: 
     color_bar_width = 1000  # width
     color_bar_height = 65  # height
     # Color bar's position within image
-    color_bar_x_coordinate = (
-        image_width - color_bar_width
-    ) // 2  # x-coordinate for placing color bar
-    color_bar_y_coordinate = (
-        image_height - color_bar_height
-    ) - 200  # y-coordinate for placing color bar
-    color_bar_position = (
-        color_bar_x_coordinate,
-        color_bar_y_coordinate,
-    )  # top-left corner of color bar
+    color_bar_x_coordinate = (image_width - color_bar_width) // 2  # x-coordinate for placing color bar
+    color_bar_y_coordinate = (image_height - color_bar_height) - 200  # y-coordinate for placing color bar
+    color_bar_position = (color_bar_x_coordinate, color_bar_y_coordinate)  # top-left corner of color bar
     # Creating color bar using matplotlib
     color_bar = np.linspace(0, 1, num_shades)
     color_bar = np.vstack((color_bar, color_bar))
     # Plotting the color bar
     plt.imshow(color_bar, aspect="auto", cmap=cmap_name)
     # plt.axis("off")  # turns off axis
-    plt.savefig(
-        "/tmp/color_bar.png", bbox_inches="tight", transparent=True, pad_inches=0
-    )  # saving color bar as PNG
+    plt.savefig("/tmp/color_bar.png", bbox_inches="tight", transparent=True, pad_inches=0)  # saving color bar as PNG
     plt.close()
     # Loading the color bar as PIL
     color_bar_image = Image.open("/tmp/color_bar.png")
     # Pasting the color bar onto the existing image
-    cell_outlines.paste(
-        color_bar_image.resize((color_bar_width, color_bar_height)), color_bar_position
-    )
+    cell_outlines.paste(color_bar_image.resize((color_bar_width, color_bar_height)), color_bar_position)
 
     return color_palette
 
 
 # Assigns a color to the specified cell based on the scaled predicted value
 # Repeat assignment for all cells (shown in draw_cell_coordinates_from_csv)
-def color_cell(
-    draw: ImageDraw,
-    color_palette: list,
-    cell: int,
-    cell_coordinates: list,
-    predicted_value_excel: str,
-):
+def color_cell(draw: ImageDraw, color_palette: list, cell: int, cell_coordinates: list, predicted_value_excel: str):
     # Color cell based on scaled predicted value (only if the cell has a predicted deformation value)
     for index, row in predicted_value_excel.iterrows():
         if cell == row[" order"]:
             # Interpolate color using scaled predicted value
-            interpolated_color_index = int(
-                row["scaled predicted cyto"] * (len(color_palette) - 1)
-            )
+            interpolated_color_index = int(row["scaled predicted cyto"] * (len(color_palette) - 1))
             # Retrieving color from color palette using the interpolated_color_index
             # Make sure each RGB value is a tuple of integers
             color = color_palette[interpolated_color_index]
@@ -305,9 +273,7 @@ def label_cell(draw: ImageDraw, font: ImageFont, cell: int, cell_coordinates: li
 
 def main():
     # Creating cell outline from .txt file
-    draw_cell_coordinates_from_txt(
-        "txt_data/nucleus_20X_zstackEDF - Cropped 1_cp_outlines.txt", 5000, 5000
-    )
+    draw_cell_coordinates_from_txt("txt_data/nucleus_20X_zstackEDF - Cropped 1_cp_outlines.txt", 5000, 5000)
 
     # Creating cell outline from .csv file + color gradient + labeling
     draw_cell_coordinates_from_csv("csv_data/pre_cyto_outlines.csv", 2075, 2200)
